9e9cfe0ee45d8c2b16e3171edb75a505b6e7dcdc
Madiha Wazir <ahmardiha@gmail.com>
2025-10-21 15:17:52 -0400

    update Footer.jsx file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,48 @@
 import "./Footer.css";
+import React from "react";
 
-function Footer() {}
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__copyright">
+          &copy; {currentYear} Supersite, Powered by News Api
+        </p>
+        <div className="footer__right-section">
+          {" "}
+          <nav className="footer__nav-links">
+            <a href="/" className="footer__link">
+              Home
+            </a>
+            <a
+              href="https://tripleten.com"
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              TripleTen
+            </a>
+          </nav>
+          <nav className="footer__social-icons">
+            <a
+              href="https://github.com"
+              className="footer__social-icon-link footer__social-icon-github"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
+            <a
+              href="https://facebook.com"
+              className="footer__social-icon-link footer__social-icon-facebook"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
