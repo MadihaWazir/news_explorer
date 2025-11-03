0d841da84648804c15a9fa1766f93eb1a9404dc6
Madiha Wazir <ahmardiha@gmail.com>
2025-11-03 12:28:41 -0500

    Update JSX and CSS

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound() {
       <img src={NotFoundPic} alt="Not Found" className="not-found__image" />
       <h2 className="not-found__title">No results found</h2>
       <p className="not-found__message">
-        Sorry, but nothing matched your search terms.
+        Sorry, but nothing matched <br /> your search terms.
       </p>
     </div>
   );
